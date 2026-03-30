93a5cee904e47a456582c2e6271becb7fdc9c894
Александр <alexandr.serch.qa@gmail.com>
2026-03-30 23:17:15 +0300

    Наполнение тестового фреймворка

--- tests/test_register_user.py
+++ tests/test_register_user.py
@@ -9,11 +9,14 @@ from models.requests.register_user_payload import RegisterUserPayload
 @allure.epic("Пользователь")
 @allure.feature("Регистрация пользователя")
 class TestRegisterUser:
-
     @allure.title("Успешная регистрация пользователя")
     def test_success_register_user(self, user_client, faker):
         password = faker.password(length=8, special_chars=False)
-        payload = RegisterUserPayload(email=faker.email(), password=password, submitPassword=password)
+        payload = RegisterUserPayload(
+            email=faker.email(), password=password, submitPassword=password
+        )
         result = user_client.register_user(payload)
 
-        assert_field(actual=result.user.email, expected=payload.email, field_name="email")
+        assert_field(
+            actual=result.user.email, expected=payload.email, field_name="email"
+        )
